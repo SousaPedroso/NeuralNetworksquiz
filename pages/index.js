1e33b31ac69cf952bf86c2a70ec91d993ecdb55f
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2021-01-26 16:51:56 -0400

    Update: Fonte e página principal

--- pages/index.js
+++ pages/index.js
@@ -4,6 +4,7 @@ import Widget from '../src/components/Widget'
 import QuizBackground from '../src/components/QuizBackground'
 import Footer from '../src/components/Footer'
 import GithubCorner from '../src/components/GithubCorner'
+import Head from 'next/head'
 
 export const QuizContainer = styled.div`
   width: 100%;
@@ -19,9 +20,13 @@ export const QuizContainer = styled.div`
 export default function Home() {
   return(
     <QuizBackground backgroundImage={db.bg}>
+      <Head>
+        <title>Neural Networks Quiz</title>
+      </Head>
       <QuizContainer>
         <Widget>
             <Widget.Header>
+              <h1>Neural Networks</h1>
             </Widget.Header>
             <Widget.Content>
             </Widget.Content>
@@ -29,11 +34,12 @@ export default function Home() {
 
         <Widget>
             <Widget.Content>
+              <h1>Quizes da galera</h1>
             </Widget.Content>
         </Widget>
         <Footer/>
       </QuizContainer>
-      <GithubCorner/>
+      <GithubCorner projectUrl="https://github.com/SousaPedroso" />
     </QuizBackground>
   )
 }
