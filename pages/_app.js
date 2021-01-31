f603af1149dc6f133d34c6116a5e1e0efbde3273
Gabriel de Sousa Gomes Pedroso <gsousa844@gmail.com>
2021-01-31 10:59:31 -0400

    Add metatags

--- pages/_app.js
+++ pages/_app.js
@@ -32,6 +32,18 @@ export default function App({ Component, pageProps }) {
   return (
     <>
     <Head>
+      <meta property="og:type" content="website" />
+      <meta property="og:url" content="https://neuralnetworksquiz.vercel.app" />
+      <meta property="og:title" content="NN Quiz | O quanto você conhece sobre Redes Neurais?" />
+      <meta property="og:description" content="Clique e teste seus conhecimentos sobre essa sub-área da Aprendizagem de Máquina tão destacada." />
+      <meta property="og:image" content={db.bg} />
+
+      <meta property="twitter:card" content="summary_large_image" />
+      <meta property="twitter:url" content="https://neuralnetworksquiz.vercel.app" />
+      <meta property="twitter:title" content="NN Quiz | O quanto você conhece sobre Redes Neurais?" />
+      <meta property="twitter:description" content="Clique e teste seus conhecimentos sobre essa sub-área da Aprendizagem de Máquina tão destacada." />
+      <meta property="twitter:image" content={db.bg} />
+
       <link rel="preconnect" href="https://fonts.gstatic.com"/>
       <link href="https://fonts.googleapis.com/css2?family=PT+Serif:ital,wght@0,400;0,700;1,400;1,700&display=swap" rel="stylesheet"/>
 
